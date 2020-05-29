21b5605a129204fdc5b8f0a94cd748350568006f
Alexandre Dufournet <alex.dufournet@guardian.co.uk>
2020-05-29 10:01:28 +0100

    Nicer paths in the tests

--- src/test/scala/com/gu/scrooge/backend/typescript/TypescriptGeneratorSpec.scala
+++ src/test/scala/com/gu/scrooge/backend/typescript/TypescriptGeneratorSpec.scala
@@ -1,7 +1,5 @@
 package com.gu.scrooge.backend.typescript
 
-import java.io.File
-import java.net.URL
 import java.nio.file.{Files, Path, Paths, StandardCopyOption}
 
 import sys.process._
@@ -11,29 +9,31 @@ import org.scalatest.matchers.should.Matchers
 
 class TypescriptGeneratorSpec extends AnyFlatSpec with Matchers {
 
-  def findFile(name: String): URL = this.getClass.getClassLoader.getResource(name)
+  def findFile(name: String): Path = Paths.get(this.getClass.getClassLoader.getResource(name).toURI)
 
-  def copy(from: String, to: String): Unit = {
-    Files.copy(Paths.get(findFile(from).toURI), Paths.get(to), StandardCopyOption.REPLACE_EXISTING)
-  }
+  def copy(from: Path, to: Path): Unit = Files.copy(from, to, StandardCopyOption.REPLACE_EXISTING)
+
+  val schoolResources: Path = findFile("school")
+  val output: Path = Paths.get("target","test")
+  val schoolPackage: Path = output.resolve("@guardian").resolve("school")
 
   "A Typescript generator" should "generate typescript" in {
     val compiler = new Compiler()
     compiler.destFolder = "target/test/"
-    compiler.thriftFiles += findFile("school/university.thrift").getFile
-    compiler.thriftFiles += findFile("school/shared.thrift").getFile
+    compiler.thriftFiles += schoolResources.resolve("university.thrift").toString
+    compiler.thriftFiles += schoolResources.resolve("shared.thrift").toString
     compiler.language = "typescript"
     compiler.run()
   }
 
   it should "compile" in {
-    copy("school/package.json", "target/test/@guardian/school/package.json")
-    copy("school/tsconfig.json", "target/test/@guardian/school/tsconfig.json")
+    copy(schoolResources.resolve("package.json"), schoolPackage.resolve("package.json"))
+    copy(schoolResources.resolve("tsconfig.json"), schoolPackage.resolve("tsconfig.json"))
 
-    val npmInstall = Process("npm install", new File("target/test/@guardian/school/")).!
+    val npmInstall = Process("npm install", schoolPackage.toFile).!
     npmInstall shouldEqual 0
 
-    val tsc = Process("tsc", new File("target/test/@guardian/school/")).!
+    val tsc = Process("tsc", schoolPackage.toFile).!
     tsc shouldEqual 0
   }
 
