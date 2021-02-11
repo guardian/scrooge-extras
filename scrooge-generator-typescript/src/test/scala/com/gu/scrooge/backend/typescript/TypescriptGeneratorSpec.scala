20c92a95036588a0303e37409c5af15d43b236c4
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2021-02-11 14:46:20 +0000

    Added TypeScript as a dev dependency
    - Added package.json and package-lock.json
    - Updated tests to use locally installed version of tsc
    - Ignored node_modules

--- scrooge-generator-typescript/src/test/scala/com/gu/scrooge/backend/typescript/TypescriptGeneratorSpec.scala
+++ scrooge-generator-typescript/src/test/scala/com/gu/scrooge/backend/typescript/TypescriptGeneratorSpec.scala
@@ -58,7 +58,7 @@ class TypescriptGeneratorSpec extends AnyFlatSpec with Matchers {
     val npmInstall = Process("npm install", npmProject.packageDirectory.toFile).!
     npmInstall shouldEqual 0
 
-    val tsc = Process("tsc", npmProject.packageDirectory.toFile).!
+    val tsc = Process("./node_modules/.bin/tsc", npmProject.packageDirectory.toFile).!
     tsc shouldEqual 0
   }
 
