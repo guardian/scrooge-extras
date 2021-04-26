eca8170839a3cc139cfbe50da0a0a87e8a982bef
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2021-04-26 18:45:28 +0100

    Bumped Thrift in built project to `0.13.0`
    - Bumped thrift types to `0.10.10`

--- scrooge-generator-typescript/src/main/scala/com/gu/scrooge/backend/typescript/NPMLibraries.scala
+++ scrooge-generator-typescript/src/main/scala/com/gu/scrooge/backend/typescript/NPMLibraries.scala
@@ -3,9 +3,9 @@ package com.gu.scrooge.backend.typescript
 object NPMLibraries {
   val dependencies = Map(
     "@types/node-int64" -> "^0.4.29",
-    "@types/thrift" -> "^0.10.9",
+    "@types/thrift" -> "^0.10.10",
     "node-int64" -> "^0.4.0",
-    "thrift" -> "^0.12.0"
+    "thrift" -> "^0.13.0"
   )
 
   val devDependencies = Map("typescript" -> "^3.8.3")
